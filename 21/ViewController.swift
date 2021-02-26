166508d1afcee742ae1eca186d4825fa90b122ae
Adrian Yip <adrianphyip@gmail.com>
2021-02-26 21:23:54 +0000

    Initial Commit

---
+++ 21/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  21
+//
+//  Created by Adrian Yip on 26/02/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
